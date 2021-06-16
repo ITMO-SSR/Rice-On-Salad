b1ffa79ad2010fe5a48b11af0739da9d257fcc46
Nikita Fedotov <nik-off.fedotoff@yandex.ru>
2021-06-16 17:46:27 +0300

    Added comments

--- programs/ros/control_node.py
+++ programs/ros/control_node.py
@@ -3,69 +3,61 @@ import rospy
 import serial
 from geometry_msgs.msg import Twist
 
-arduino_name = "/dev/ttyACM0"
+arduino_name = "/dev/ttyACM0" # Порт, через который происходит взаимодействие с Arduino
 
-serial_port = serial.Serial(arduino_name, 9600)
-serial_port.flush()
+serial_port = serial.Serial(arduino_name, 9600) # Подключаемся по UART к Arduino
+serial_port.flush() # Очищаем канал передачи данных
 
-def bound(value):
+def bound(value): # Функция, возвращающая закодированное значение скорости
   upper_bound = 100
 
-  sign = "1" if value >= 0 else "0"
+  sign = "1" if value >= 0 else "0" # 1 - движение вперед, 0 - движение назад
   absolute = str(abs(value)) if abs(value) < upper_bound else str(upper_bound)
 
-  # Add missing digits
-  absolute = "0" * (3 - len(absolute)) + absolute
+  absolute = "0" * (3 - len(absolute)) + absolute # Добавляем недостающие нули
 
   return sign + absolute
 
-def encode(linear, angular):
+def encode(linear, angular): # Функция для расчета сигнала, передаваемого на Arduino
   left = "1000"
   right = "1000"
 
   direct_speed = 90
 
-  if angular == 0:
+  if angular == 0: # Робот едет прямо
     left = right = bound(linear)
   elif angular > 0:
-    if linear == 0:
+    if linear == 0: # Робот поворачивается вокруг своей оси вправо
       left = bound(direct_speed)
-    else:
+    else: # Робот движется с уклоном влево
       left = bound(linear - angular)
       right = bound(linear + angular)
   else:
-    if linear == 0:
+    if linear == 0: # Робот поворачивается вокруг своей оси влево
       right = bound(direct_speed)
-    else:
+    else: # Робот движется с уклоном вправо
       left = bound(linear + angular)
       right = bound(linear - angular)
 
   return left + right
 
-def callback(msg):
-  # Linear and angular velocities
-  linear_vel = int(msg.linear.x / 0.01)
-  angular_vel = int(msg.angular.z / 0.1)
+def callback(msg): # Коллбэк, который выполняется при получении новых сообщений из топика
+  linear_vel = int(msg.linear.x / 0.01) # Линейная скорость
+  angular_vel = int(msg.angular.z / 0.1) # Угловая скорость
 
   rospy.loginfo("Received: lin = %f, ang = %f", linear_vel, angular_vel)
 
-  num = encode(linear_vel, angular_vel)
+  num = encode(linear_vel, angular_vel) # Закодированные скорости, передаваемые на Arduino
   encoded_str = num + "\n"
 
   rospy.loginfo(encoded_str)
   serial_port.write(encoded_str)
 
 def listener():
-  # Init our node
-  rospy.init_node('control_node')
-
-  # Subscribe to the cmd_vel
-  rospy.Subscriber("/cmd_vel", Twist, callback)
-
+  rospy.init_node('control_node') # Инициализируем ROS-ноду
+  rospy.Subscriber("/cmd_vel", Twist, callback) # Подписываемся на топик cmd_vel
   rospy.loginfo("control_node started working")
-
-  # Keep it alive
-  rospy.spin()
+  rospy.spin() # Нужно, чтобы ROS не завершил работу раньше времени
 
 if __name__ == '__main__':
   try:
--- programs/ros/distance_node.py
+++ programs/ros/distance_node.py
@@ -4,21 +4,21 @@ import rospy
 import serial
 from sensor_msgs.msg import LaserScan
 
-arduino_name = "/dev/ttyACM0"
+arduino_name = "/dev/ttyACM0" # Порт, через который происходит взаимодействие с Arduino
 
-serial_port = serial.Serial(arduino_name, 9600)
-serial_port.flush()
+serial_port = serial.Serial(arduino_name, 9600) # Подключаемся по UART к Arduino
+serial_port.flush() # Очищаем канал передачи данных
 
-dist_forward_min = 0.3
-dist_backward_min = 0.3
-dist_medium = 0.35
+dist_forward_min = 0.3 # Минимальное расстояние до ближайшего предмета спереди
+dist_backward_min = 0.3 # Минимальное расстояние до ближайшего предмета сзади
+dist_medium = 0.35 # Если расстояние до ближайшего объекта меньше данной, то робот снижает скорость
 
-angle_forward = 0.4
-angle_backward = 0.5
+angle_forward = 0.4 # Половина угла обзора спереди
+angle_backward = 0.5 # Половина угла обзора сзади
 
-state_cur = "0"
+state_cur = "0" # Текущее состояние
 
-def find_min(ranges):
+def find_min(ranges): # Функция, возвращающая минимальное расстояние и его индекс в списке ranges
   min_current = 1000000
   index_current = -1
   for i, e in list(enumerate(ranges)):
@@ -27,43 +27,43 @@ def find_min(ranges):
       index_current = i
   return (index_current, min_current)
 
-def compute_signal(index, range, msg):
-  angle_min = msg.angle_min
-  angle_max = msg.angle_max
-  angle_current = (angle_max - angle_min) / len(msg.ranges) * index
+def compute_signal(index, range, msg): # Функция расчета текущего состояния
+  angle_min = msg.angle_min # Начальный угол сканирования
+  angle_max = msg.angle_max # Конечный угол сканирования
+  angle_current = (angle_max - angle_min) / len(msg.ranges) * index # Угол, соответствующий ближайшему объекту
 
-  if range > dist_forward_min and range < dist_medium:
+  if range > dist_forward_min and range < dist_medium: # Снижаем скорость
     signal = "4"
-  elif range < dist_forward_min and abs(angle_current) < angle_forward:
+  elif range < dist_forward_min and abs(angle_current) < angle_forward: # Вперед ехать нельзя
     signal = "1"
-  elif range < dist_backward_min and abs(math.pi - angle_current) < angle_backward:
+  elif range < dist_backward_min and abs(math.pi - angle_current) < angle_backward: # Назад ехать нельзя
     signal = "2"
-  else:
+  else: # Можно спокойно двигаться
     signal = "0"
 
   return signal
 
-def callback(msg):
+def callback(msg): # Коллбэк, который выполняется при получении новых сообщений из топика
   global state_cur
 
-  ranges = msg.ranges
+  ranges = msg.ranges # расстояния до объектов, полученные с лидара
   tup = find_min(ranges)
   rospy.loginfo("Received: index = %f, range = %f", tup[0], tup[1])
 
   signal = compute_signal(tup[0], tup[1], msg)
   rospy.loginfo("Signal = " + signal)
 
-  if signal != state_cur:
+  if signal != state_cur: # Если состояние изменилось, передаем сообщение на Arduino
     rospy.loginfo("New state = " + signal)
     state_cur = signal
     encoded_str = signal + "#"
     serial_port.write(encoded_str)
 
 def listener():
-  rospy.init_node("distance_node")
-  rospy.Subscriber("/scan", LaserScan, callback)
+  rospy.init_node("distance_node") # Инициализируем ROS-ноду
+  rospy.Subscriber("/scan", LaserScan, callback) # Подписываемся на топик /scan
   rospy.loginfo("distance_node started working")
-  rospy.spin()
+  rospy.spin() # Нужно, чтобы ROS не завершил работу раньше времени
 
 if __name__ == '__main__':
   try:
